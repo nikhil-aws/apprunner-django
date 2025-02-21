afecce14ee8eb305e3dc8aac90310ca8b53428d0
Nikhil <86740036+nikhil-aws@users.noreply.github.com>
2025-02-21 13:47:58 +0530

    Update wsgi.py

--- attc_website/attc_website/wsgi.py
+++ attc_website/attc_website/wsgi.py
@@ -3,7 +3,7 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 try:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attc_website.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attc_website.attc_website.settings')
     application = get_wsgi_application()
 except Exception as e:
     print(f"Error loading WSGI application: {e}", file=sys.stderr)
