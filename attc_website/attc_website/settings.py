cd5ef2f1a6e811f3283f43d562d584fa1199a703
Nikhil <86740036+nikhil-aws@users.noreply.github.com>
2025-02-21 09:57:16 +0530

    Update settings.py

--- attc_website/attc_website/settings.py
+++ attc_website/attc_website/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'attc_website.attc_website.urls'
+ROOT_URLCONF = 'attc_website.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'attc_website.attc_website.wsgi.application'
+WSGI_APPLICATION = 'attc_website.wsgi.application'
 
 
 # Database
