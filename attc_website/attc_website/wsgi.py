4534bd0c1f9b516d8088e2946c3eb71ffbfed167
Nikhil <86740036+nikhil-aws@users.noreply.github.com>
2025-02-21 11:06:55 +0530

    Update wsgi.py

--- attc_website/attc_website/wsgi.py
+++ attc_website/attc_website/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for attc_website project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attc_website.settings')
-
-application = get_wsgi_application()
+try:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attc_website.settings')
+    application = get_wsgi_application()
+except Exception as e:
+    print(f"Error loading WSGI application: {e}", file=sys.stderr)
+    raise
