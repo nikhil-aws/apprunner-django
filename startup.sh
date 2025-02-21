a4cfb4676319c1baa4de1e043cddac27ca35590c
Nikhil <86740036+nikhil-aws@users.noreply.github.com>
2025-02-21 11:32:15 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 attc_website.attc_website.wsgi
+python ./attc_website/manage.py collectstatic && gunicorn --workers 2 attc_website.attc_website.wsgi
