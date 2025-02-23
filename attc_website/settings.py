130353c7b364ec3019ad72d7c3328c3561cc313f
Nikhil <86740036+nikhil-aws@users.noreply.github.com>
2025-02-23 11:04:46 +0530

    Update settings.py

--- attc_website/settings.py
+++ attc_website/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'attc_app',
+    'attc_website.attc_app',
 ]
 
 MIDDLEWARE = [
